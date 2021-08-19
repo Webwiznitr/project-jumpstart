5dd7ca160f0e8934b544eea5679e77bdf6f26ae6
Aniket762 <aniketindian8@gmail.com>
2021-08-20 02:14:32 +0530

    Feat: 404 page init

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,15 @@
-import * as React from "react"
+import * as React from 'react'
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from '../components/layout'
+import Seo from '../components/seo'
+import { Heading, SubHeading } from '../components/theme'
 
 const NotFoundPage = () => (
-  <Layout>
-    <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <Layout>
+        <Seo title="404: Not found" />
+        <Heading>404 </Heading>
+        <Heading>Awww.. Don't cry 🐣</Heading>
+    </Layout>
 )
 
 export default NotFoundPage
