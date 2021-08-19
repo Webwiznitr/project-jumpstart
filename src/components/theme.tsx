9bb08d93fd2c716ff0f0c1a411d3600f432e285e
Aniket762 <aniketindian8@gmail.com>
2021-08-19 23:43:18 +0530

    minor

--- src/components/theme.tsx
+++ src/components/theme.tsx
@@ -12,10 +12,15 @@ export const Landing = styled.div`
 `
 export const About = styled.p`
     color: grey;
-
     font-size: 1.3rem;
     line-height: 2rem;
     letter-spacing: 0.1rem;
     margin: 5% auto;
     width: 70%;
+`
+export const Partners = styled.div``
+
+export const SubHeading = styled.h3`
+    color: #fff;
+    font-weight: 700;
 `
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,9 @@ import { Link } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
-import { About, Heading, Landing } from '../components/theme'
+import { About, Heading, Landing, Partners, SubHeading } from '../components/theme'
 import Workshop from '../components/workshop'
+import { Grid } from 'gatsby-plugin-material-ui'
 
 const IndexPage = () => (
     <Layout>
@@ -22,7 +23,10 @@ const IndexPage = () => (
                 or a typeface without relying on meaningful content.In publishing and graphic design, Lorem ipsum is a placeholder text
                 commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.
             </About>
-            <Workshop />
+            <Partners>
+                <h3>Some cool subheading sochenge</h3>
+                <SubHeading>Some cool subheading sochenge</SubHeading>
+            </Partners>
         </Landing>
     </Layout>
 )
