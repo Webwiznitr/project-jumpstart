f2c9642426adc328e6ed60a070586c0afaab73d8
Sumana Basu <sumana.basu2001@gmail.com>
2021-08-20 00:53:39 +0530

    Feat: Schedule

---
+++ src/components/schedule.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Day1, Day2, Day3 } from './ScheduleData'
+import ScheduleCard from './ScheduleCard'
+
+function card1(Day1) {
+    return <ScheduleCard key={Day1.id} time={Day1.time} date={Day1.date} title={Day1.title} />
+}
+function card2(Day2) {
+    return <ScheduleCard key={Day2.id} time={Day2.time} date={Day2.date} title={Day2.title} />
+}
+
+function card3(Day3) {
+    return <ScheduleCard key={Day3.id} time={Day3.time} date={Day3.date} title={Day3.title} />
+}
+
+function Schedule() {
+    return (
+        <div className="container-schedule">
+            <section className="content-section">
+                <div className="contents">
+                    <div id="day1" className="daily-schedule">
+                        {Day1.map(card1)}
+                        {Day2.map(card2)}
+                        {Day3.map(card3)}
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Schedule
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ import { StaticImage } from 'gatsby-plugin-image'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 import { About, Heading, Landing, Partners, SubHeading } from '../components/theme'
-import Workshop from '../components/workshop'
 import { Grid } from '@material-ui/core'
+import Schedule from '../components/schedule'
 
 const IndexPage = () => (
     <Layout>
@@ -23,30 +23,56 @@ const IndexPage = () => (
                 or a typeface without relying on meaningful content.In publishing and graphic design, Lorem ipsum is a placeholder text
                 commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.
             </About>
+            <SubHeading>Some cool subheading sochenge 🚀</SubHeading>
+            <Schedule />
             <Partners>
                 <h3>Some cool subheading sochenge</h3>
                 <SubHeading>Some cool subheading sochenge</SubHeading>
                 <div className="grid">
-                <Grid container spacing={0}>
-                    <Grid item xs={6} sm={4} md={3} lg={2}>
-                        <img className="grid_item" src="https://res.cloudinary.com/webwiznitr/image/upload/v1628961683/Assets/Sponsors/GitHub_Logo_White_lmfoza.png" alt="logo" />
+                    <Grid container spacing={0}>
+                        <Grid item xs={6} sm={4} md={3} lg={2}>
+                            <img
+                                className="grid_item"
+                                src="https://res.cloudinary.com/webwiznitr/image/upload/v1628961683/Assets/Sponsors/GitHub_Logo_White_lmfoza.png"
+                                alt="logo"
+                            />
+                        </Grid>
+                        <Grid item xs={6} sm={4} md={3} lg={2}>
+                            <img
+                                className="grid_item"
+                                src="https://res.cloudinary.com/webwiznitr/image/upload/v1628961683/Assets/Sponsors/GitHub_Logo_White_lmfoza.png"
+                                alt="logo"
+                            />
+                        </Grid>
+                        <Grid item xs={6} sm={4} md={3} lg={2}>
+                            <img
+                                className="grid_item"
+                                src="https://res.cloudinary.com/webwiznitr/image/upload/v1628961683/Assets/Sponsors/GitHub_Logo_White_lmfoza.png"
+                                alt="logo"
+                            />
+                        </Grid>
+                        <Grid item xs={6} sm={4} md={3} lg={2}>
+                            <img
+                                className="grid_item"
+                                src="https://res.cloudinary.com/webwiznitr/image/upload/v1628961683/Assets/Sponsors/GitHub_Logo_White_lmfoza.png"
+                                alt="logo"
+                            />
+                        </Grid>
+                        <Grid item xs={6} sm={4} md={3} lg={2}>
+                            <img
+                                className="grid_item"
+                                src="https://res.cloudinary.com/webwiznitr/image/upload/v1628961683/Assets/Sponsors/GitHub_Logo_White_lmfoza.png"
+                                alt="logo"
+                            />
+                        </Grid>
+                        <Grid item xs={6} sm={4} md={3} lg={2}>
+                            <img
+                                className="grid_item"
+                                src="https://res.cloudinary.com/webwiznitr/image/upload/v1628961683/Assets/Sponsors/GitHub_Logo_White_lmfoza.png"
+                                alt="logo"
+                            />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={6} sm={4} md={3} lg={2}>
-                        <img className="grid_item" src="https://res.cloudinary.com/webwiznitr/image/upload/v1628961683/Assets/Sponsors/GitHub_Logo_White_lmfoza.png" alt="logo" />
-                    </Grid>
-                    <Grid item xs={6} sm={4} md={3} lg={2}>
-                        <img className="grid_item" src="https://res.cloudinary.com/webwiznitr/image/upload/v1628961683/Assets/Sponsors/GitHub_Logo_White_lmfoza.png" alt="logo" />
-                    </Grid>
-                    <Grid item xs={6} sm={4} md={3} lg={2}>
-                        <img className="grid_item" src="https://res.cloudinary.com/webwiznitr/image/upload/v1628961683/Assets/Sponsors/GitHub_Logo_White_lmfoza.png" alt="logo" />
-                    </Grid>
-                    <Grid item xs={6} sm={4} md={3} lg={2}>
-                        <img className="grid_item" src="https://res.cloudinary.com/webwiznitr/image/upload/v1628961683/Assets/Sponsors/GitHub_Logo_White_lmfoza.png" alt="logo" />
-                    </Grid>
-                    <Grid item xs={6} sm={4} md={3} lg={2}>
-                        <img className="grid_item" src="https://res.cloudinary.com/webwiznitr/image/upload/v1628961683/Assets/Sponsors/GitHub_Logo_White_lmfoza.png" alt="logo" />
-                    </Grid>
-                </Grid>
                 </div>
             </Partners>
         </Landing>
