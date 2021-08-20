cf5a846a960fb21dc64208f9833a34c26cec1f89
= <aabhassao0@gmail.com>
2021-08-20 21:52:10 +0530

    Timeline UI Change
    Signed-off-by: = <aabhassao0@gmail.com>

--- src/components/Schedule/ScheduleData.tsx
+++ src/components/Schedule/ScheduleData.tsx
@@ -4,48 +4,56 @@ const Day1 = [
         time: '9:00',
         date: '18 Sept',
         title: 'Opening Ceremony',
+        subtitle: '',
     },
     {
         id: 2,
         time: '10:00',
         date: '18 Sept',
         title: 'Hackathon Begins',
+        subtitle: '',
     },
     {
         id: 3,
         time: '12:00',
         date: '18 Sept',
-        title: 'Talk by Dr. Munish Jindal, CEO of HoverRobotix',
+        title: 'Talk by Dr. Munish Jindal',
+        subtitle: 'CEO of HoverRobotix'
     },
     {
         id: 4,
         time: '15:00',
         date: '18 Sept',
         title: 'Brainstorming session',
+        subtitle: '',
     },
     {
         id: 5,
         time: '18:00',
         date: '18 Sept',
         title: 'Dino game',
+        subtitle: '',
     },
     {
         id: 6,
         time: '20:00',
         date: '18 Sept',
         title: 'Initial Submission',
+        subtitle: '',
     },
     {
         id: 7,
         time: '20:30',
         date: '18 Sept',
         title: 'Game Time',
+        subtitle: '',
     },
     {
         id: 8,
         time: '22:30',
         date: '18 Sept',
-        title: 'Workshop by Koren Grinshpoon, CEO of EchoAR',
+        title: 'Workshop by Koren Grinshpoon',
+        subtitle: 'CEO of EchoAR',
     },
 ]
 
@@ -55,36 +63,42 @@ const Day2 = [
         time: '10:00',
         date: '19 Sept',
         title: 'Game Time',
+        subtitle: '',
     },
     {
         id: 10,
         time: '17:00',
         date: '19 Sept',
         title: 'Final Project Submission',
+        subtitle: '',
     },
     {
         id: 11,
         time: '17:30',
         date: '19 Sept',
         title: 'Project Showcase',
+        subtitle: '',
     },
     {
         id: 12,
         time: '19:00',
         date: '19 Sept',
         title: 'Top 10 announcement',
+        subtitle: '',
     },
     {
         id: 13,
         time: '19:30',
         date: '19 Sept',
         title: 'Presentation of top projects',
+        subtitle: '',
     },
     {
         id: 14,
         time: '20:30',
         date: '19 Sept',
         title: 'Closing Ceremony',
+        subtitle: '',
     },
 ]
 
@@ -94,6 +108,7 @@ const Day3 = [
         time: '19:00',
         date: '22 Sept',
         title: 'Winners announcement',
+        subtitle: '',
     },
 ]
 
