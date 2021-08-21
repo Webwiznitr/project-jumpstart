3efc74924390e07af2f24d1a4e44138db86a8a64
Mohan Kumar S <mohankumar27197@gmail.com>
2021-08-21 12:46:00 +0530

    initial gif loading bug fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,9 @@ import Content from '../components'
 import Preloader from '../components/Preloader'
 
 export default function Index() {
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        setLoading(true)
         setTimeout(() => {
             setLoading(false)
         }, 3000)
